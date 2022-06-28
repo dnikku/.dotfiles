39a0080d07aba67f791ab12cc49a6763ca0d7ce5
Nicolae Dascalu <n.dascalu@ceyoniq.com>
2022-06-28 17:39:23 +0200

    start/kill intellij 2022.3 from snap/ubuntu

--- kill-idea.sh
+++ kill-idea.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IDEA_PIDS=$(ps aux | grep ideaIU/jbr/bin/java | grep -v grep | awk '{printf "%s ", $2}')
+IDEA_PIDS=$(ps aux | grep '/intellij-idea-ultimate/348/jbr/bin/java' | grep -v grep | awk '{printf "%s ", $2}')
 
 if [[ "$IDEA_PIDS" != "" ]]; then
   echo "Killing idea PID=$IDEA_PIDS"
