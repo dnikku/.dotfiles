bccb3f3436bfbb23a308fc0b2d72ed3e4a66fe0b
Nicu Dascalu <nicolae-florin.dascalu@bertelsmann.de>
2024-06-08 10:48:36 +0200

    fix color

--- nvim/lua/josean/plugins/lualine.lua
+++ nvim/lua/josean/plugins/lualine.lua
@@ -7,38 +7,38 @@ return {
 
     local colors = {
       blue = "#65D1FF",
-      green = "#3EFFDC",
+      green = "#A0FFB0",
       violet = "#FF61EF",
       yellow = "#FFDA7B",
       red = "#FF4A4A",
-      fg = "#c3ccdc",
-      bg = "#112638",
-      inactive_bg = "#2c3043",
+      fg = "#121212",
+      bg = "#9FDFBF",
+      inactive_bg = "#E0EBEB",
     }
 
     local my_lualine_theme = {
       normal = {
-        a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
+        a = { bg = colors.blue, fg = colors.fg },
         b = { bg = colors.bg, fg = colors.fg },
         c = { bg = colors.bg, fg = colors.fg },
       },
       insert = {
-        a = { bg = colors.green, fg = colors.bg, gui = "bold" },
+        a = { bg = colors.green, fg = colors.fg, gui = "bold" },
         b = { bg = colors.bg, fg = colors.fg },
         c = { bg = colors.bg, fg = colors.fg },
       },
       visual = {
-        a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
+        a = { bg = colors.violet, fg = colors.fg, gui = "bold" },
         b = { bg = colors.bg, fg = colors.fg },
         c = { bg = colors.bg, fg = colors.fg },
       },
       command = {
-        a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
+        a = { bg = colors.yellow, fg = colors.fg, gui = "bold" },
         b = { bg = colors.bg, fg = colors.fg },
         c = { bg = colors.bg, fg = colors.fg },
       },
       replace = {
-        a = { bg = colors.red, fg = colors.bg, gui = "bold" },
+        a = { bg = colors.red, fg = colors.fg, gui = "bold" },
         b = { bg = colors.bg, fg = colors.fg },
         c = { bg = colors.bg, fg = colors.fg },
       },
@@ -59,7 +59,7 @@ return {
           {
             lazy_status.updates,
             cond = lazy_status.has_updates,
-            color = { fg = "#ff9e64" },
+            color = { fg = "#131313" },
           },
           { "encoding" },
           { "fileformat" },
