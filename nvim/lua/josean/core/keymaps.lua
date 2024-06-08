cc2cd5c7a01e087b64263f8a250d49211ecedc8c
Nicu Dascalu <nicolae-florin.dascalu@bertelsmann.de>
2024-06-08 09:09:07 +0200

    disable lazygit as not need for now

--- nvim/lua/josean/core/keymaps.lua
+++ nvim/lua/josean/core/keymaps.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap -- for conciseness
 
+
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
@@ -21,3 +22,7 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- TODO: learn recording
+keymap.set("n", "q", "<Nop>", { desc = "Disable recording till I will learn to use it" })
+keymap.set("i", "q", "<Nop>", { desc = "Disable recording till I will learn to use it" })
