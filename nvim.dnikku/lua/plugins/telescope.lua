5df0d93247880e8aae5cdcbf770ac4e7ee734281
Nicu Dascalu <nicu.dascalu@gmail.com>
2024-09-04 13:12:04 +0200

    telescope: ignore some dirs

--- nvim.dnikku/lua/plugins/telescope.lua
+++ nvim.dnikku/lua/plugins/telescope.lua
@@ -36,7 +36,13 @@ return {
           preview_cutoff = 120,
         },
         file_sorter = require("telescope.sorters").get_fuzzy_file,
-        file_ignore_patterns = { "node_modules" },
+        file_ignore_patterns = {
+          ".git",
+          "node_modules",
+          ".mypy_cache", ".pytest_cache", "__pycache__",
+          ".coverage",
+          ".output"
+        },
         generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
         path_display = { "truncate" },
         winblend = 0,
